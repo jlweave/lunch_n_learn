60f49af06bed6bef83423a6e31424fa84eddda92
Lacey Weaver <jlweave89@gmail.com>
2023-03-04 15:19:12 -0700

    Add start of user story 3

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UserController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if @user.save
+      render json: UserSerializer.new(user), status: 201
+    else 
+      render json: {error: "Error! New User was not created!"}, status: 400
+    end
+  end
+
+
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
+end
---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Users API" do
+  describe "Users Story 3 - User Registration" do
+    
+  end
+end
