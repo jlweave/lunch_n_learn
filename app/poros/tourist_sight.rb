268a9806aa16bb67f6092d182cc382aa53df29f3
Lacey Weaver <jlweave89@gmail.com>
2023-03-06 11:54:25 -0700

    Add passing poros test

--- app/poros/tourist_sight.rb
+++ app/poros/tourist_sight.rb
@@ -2,6 +2,7 @@ class TouristSight
   attr_reader :name, :address, :place_id
 
   def initialize(attr)
+    # require 'pry'; binding.pry
     @name = attr[:features].first[:properties][:name]
     @address = attr[:features].first[:properties][:formatted]
     @place_id = attr[:features].first[:properties][:place_id]
--- spec/poros/tourist_sight_poros_spec.rb
+++ spec/poros/tourist_sight_poros_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Tourist Sight Poros" do
-  xit "exists with attributes" do
-    require 'pry'; binding.pry
-    data = {
-          properties: {
-            name: "Chipmunk Beaut Pageant", 
-            formatted: " Whoknows Rd Whoville, Co, 12345678", 
-            place_id: "1234567890"}}
+  it "exists with attributes" do
+    # require 'pry'; binding.pry
+    data = {:type=>"FeatureCollection",
+            :features=>
+              [{:type=>"Feature",
+                :properties=>
+                {:name=>"Chipmunk Beaut Pageant",
+                  :formatted=> " Whoknows Rd Whoville, Co, 12345678", 
+                  :place_id=> "1234567890"}}]}
     sight = TouristSight.new(data)
-
+# require 'pry'; binding.pry
     expect(sight).to be_an_instance_of(TouristSight)
     expect(sight.name).to eq("Chipmunk Beaut Pageant")
-    expect(sight.formatted).to eq(" Whoknows Rd Whoville, Co, 12345678")
+    expect(sight.address).to eq(" Whoknows Rd Whoville, Co, 12345678")
     expect(sight.place_id).to eq("1234567890")
   end
 end
