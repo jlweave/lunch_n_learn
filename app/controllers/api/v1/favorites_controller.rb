4cc27e7151bd8f3b3772d9cb8b68544fca17bcb2
Lacey Weaver <jlweave89@gmail.com>
2023-03-07 09:22:55 -0700

    Add removal of binding pry

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,10 +2,7 @@ class Api::V1::FavoritesController < ApplicationController
    before_action :find_user, only: [:create, :index]
    
   def create
-    # require 'pry'; binding.pry
      if @user
-      # require 'pry'; binding.pry
-  
       if @user.favorites.create!(favorite_params).save
         render json: { "success": "Favorite was added to user" }, status: 201
       end
@@ -15,9 +12,7 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   def index
-    # require 'pry'; binding.pry
     if @user
-      # require 'pry'; binding.pry
       render json: FavoritesSerializer.new(@user.favorites), status:201
     else
       render json: { "errors": "User could not be found, try again." }, status: 400
@@ -26,12 +21,10 @@ class Api::V1::FavoritesController < ApplicationController
 
   private
   def favorite_params
-    # require 'pry'; binding.pry
     params.permit(:user_api_key, :country, :recipe_link, :recipe_title)
   end
 
   def find_user
-    # require 'pry'; binding.pry
     @user = User.find_by(user_api_key: params[:user_api_key])
   end
 end
